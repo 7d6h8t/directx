1ca4aa2701f401bc3330b4192c68be2ec6f5883f
7d6h8t <7d6h8t@gmail.com>
2024-03-25 22:58:36 +0900

    update game

--- src/directx_tutorial/game.cpp
+++ src/directx_tutorial/game.cpp
@@ -5,7 +5,11 @@ Game::Game() {}
 
 Game::~Game() {}
 
-void Game::Init(HWND hwnd) {}
+void Game::Init(HWND hwnd) {
+  hwnd_ = hwnd;
+  width_ = g_win_size_x;
+  height_ = g_win_size_y;
+}
 
 void Game::Update() {}
 
--- src/directx_tutorial/game.h
+++ src/directx_tutorial/game.h
@@ -7,4 +7,11 @@ class Game {
   void Init(HWND hwnd);
   void Update();
   void Render();
+
+ private:
+  HWND hwnd_;
+  uint32_t width_ = 0;
+  uint32_t height_ = 0;
+
+  // dx
 };
