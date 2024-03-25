87daf41a812ef5e4ca57995802384382e3db161a
7d6h8t <7d6h8t@gmail.com>
2024-03-25 19:45:03 +0900

    + initial headers

--- src/directx_tutorial/pch.h
+++ src/directx_tutorial/pch.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "types.h"
+#include "values.h"
+#include "struct.h"
+
+// stl
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+
+// win
+#include <Windows.h>
+#include <assert.h>
+
+// dx
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
---
+++ src/directx_tutorial/values.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "types.h"
+
+const int32_t g_win_size_x = 800;
+const int32_t g_win_size_y = 600;
