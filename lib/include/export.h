408805a045b808a1734828e8023ed2b3097b9732
7d6h8t <7d6h8t@gmail.com>
2024-04-04 22:31:57 +0900

    - capture_control

--- lib/include/export.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if defined(CAPTURECONTROL_EXPORTS)  // inside DLL
-#define CAPTURE_API __declspec(dllexport)
-#else  // outside DLL
-#define CAPTURE_API __declspec(dllimport)
-#endif  // CAPTURECONTROL_EXPORTS
